79d0e2fb865580cee0b484e54a46b35749794f8f
c3h3 <c3h3.tw@gmail.com>
2015-09-17 18:19:24 +0800

    add install script

---
+++ installPackages.R
@@ -0,0 +1,7 @@
+
+install.packages("adabag")
+install.packages("e1071")
+
+install.packages("PerformanceAnalytics")
+install.packages("foreach")
+install.packages("quantstrat", repos="http://R-Forge.R-project.org")
