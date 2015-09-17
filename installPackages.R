9e5559ab5e60306efb1b594760ebd42e206275c9
Chia-Chi Chang <c3h3.tw@gmail.com>
2015-09-17 18:56:55 +0800

    Update installPackages.R

--- installPackages.R
+++ installPackages.R
@@ -1,7 +1,11 @@
 
-install.packages("adabag")
+#install.packages("ada")
+#install.packages("adabag")
 install.packages("e1071")
 
 install.packages("PerformanceAnalytics")
 install.packages("foreach")
 install.packages("quantstrat", repos="http://R-Forge.R-project.org")
+
+install.packages("httr")
+install.packages("rvest")
